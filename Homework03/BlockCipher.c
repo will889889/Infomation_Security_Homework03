12f65eabbcc49822d02b3ccd9f2778911e32817c
ian287913 <45512766+ian287913@users.noreply.github.com>
2019-11-29 20:18:29 +0800

    ECB

--- Homework03/BlockCipher.c
+++ Homework03/BlockCipher.c
@@ -3,37 +3,49 @@
 
 void BC_ECB_encrypt(unsigned char *buffer, int length, unsigned char *key)
 {
-	//AES_BLOCKLEN
 	struct AES_ctx ctx;
+	//	set key
 	AES_init_ctx(&ctx, key);
 
-	for (int i = 0; i < 16; i++)
+	unsigned char buf16[AES_BLOCKLEN];
+	//	ignore the last block if the size isn't 16
+	for (int i = 0; (i + AES_BLOCKLEN) < length; i+=16)
+	{
+		for (int b = 0; b < AES_BLOCKLEN; b++)
+		{
+			buf16[b] = buffer[i + b];
+		}
+		AES_ECB_encrypt(&ctx, buf16);
+		for (int b = 0; b < AES_BLOCKLEN; b++)
+		{
+			buffer[i + b] = buf16[b];
+		}
+	}
+	/*for (int i = 0; i < 16; i++)
 	{
 		printf("%02hhx ", buffer[i]);
-	}
-	printf("\n");
-	AES_ECB_encrypt(&ctx, buffer);
-
-	for (int i = 0; i < 16; i++)
-	{
-		printf("%02hhx ", buffer[i]);
-	}
+	}*/
 }
 
 void BC_ECB_decrypt(unsigned char *buffer, int length, unsigned char *key)
 {
 	struct AES_ctx ctx;
+	//	set key
 	AES_init_ctx(&ctx, key);
 
-	for (int i = 0; i < 16; i++)
+	unsigned char buf16[AES_BLOCKLEN];
+	//	ignore the last block if the size isn't 16
+	for (int i = 0; (i + AES_BLOCKLEN) < length; i += 16)
 	{
-		printf("%02hhx ", buffer[i]);
-	}
-	printf("\n");
-	AES_ECB_decrypt(&ctx, buffer);
-	for (int i = 0; i < 16; i++)
-	{
-		printf("%02hhx ", buffer[i]);
+		for (int b = 0; b < AES_BLOCKLEN; b++)
+		{
+			buf16[b] = buffer[i + b];
+		}
+		AES_ECB_decrypt(&ctx, buf16);
+		for (int b = 0; b < AES_BLOCKLEN; b++)
+		{
+			buffer[i + b] = buf16[b];
+		}
 	}
 }
 
--- Homework03/Main.c
+++ Homework03/Main.c
@@ -1,5 +1,3 @@
-#include <stdio.h>
-#include <string.h>
 #include "BlockCipher.h"
 #include "PPMReader.h"
 
@@ -19,20 +17,6 @@ int main(void)
 	printf("read ppm done");
 	writePPM("EncryptrdGunter.ppm", image);
 
-#ifndef AES256
-#error "Need to use AES256"
-#endif
-	//{ // Do encryption 
-	//	struct AES_ctx ctx;
-	//	fp = NULL;
-	//	fopen_s(fp, "aes.c", "r");
-	//	ret = fread(buf, 1, sizeof(buf), fp);
-	//	fclose(fp);
-	//	fprintf(stderr, "Raw File len=%d, will gen aes.c.enc\n", ret);
-
-	printf("\n\n");
-
-	BC_ECB_decrypt(buf, key);
 	
 	system("pause");
 	return 0;
